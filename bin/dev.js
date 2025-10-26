2a269bc322ae0dd11423532fd33a918ac6691ae5
Sebastian Freund <abes140377@web.de>
2025-10-26 09:45:56 +0100

    fix(dev): suppress NODE_TLS_REJECT_UNAUTHORIZED warning
    Filter out the security warning emitted when TLS certificate
    verification is disabled during development. This provides cleaner
    command output while maintaining the same behavior.

--- bin/dev.js
+++ bin/dev.js
@@ -1,5 +1,17 @@
-#!/usr/bin/env -S node --loader ts-node/esm --disable-warning=ExperimentalWarning
+#!/usr/bin/env -S node --loader ts-node/esm --no-warnings
 
 import {execute} from '@oclif/core'
+
+// Suppress the NODE_TLS_REJECT_UNAUTHORIZED warning in development
+const originalEmit = process.emit.bind(process)
+process.emit = function (event, ...args) {
+  if (
+    event === 'warning' &&
+    args[0]?.message?.includes('NODE_TLS_REJECT_UNAUTHORIZED')
+  ) {
+    return false
+  }
+  return originalEmit(event, ...args)
+}
 
 await execute({development: true, dir: import.meta.url})
